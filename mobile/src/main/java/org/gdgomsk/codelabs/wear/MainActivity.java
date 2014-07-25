ba602c9c2e4a605ed20956f3934934d019034d7a
Alex Korovyansky <korovyansk@gmail.com>
2014-07-26 01:25:45 +0700

    Step #2 - Bumping simple notification

--- mobile/src/main/java/org/gdgomsk/codelabs/wear/MainActivity.java
+++ mobile/src/main/java/org/gdgomsk/codelabs/wear/MainActivity.java
@@ -1,7 +1,12 @@
 package org.gdgomsk.codelabs.wear;
 
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 import android.support.v7.app.ActionBarActivity;
+import android.view.View;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -10,6 +15,36 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.main_bump_notification).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                bumpNotification();
+            }
+        });
+    }
+
+    private void bumpNotification() {
+        int notificationId = 001;
+        // Build intent for notification content
+        Intent viewIntent = new Intent(this, MainActivity.class);
+        viewIntent.putExtra("secretValue", 42);
+        PendingIntent viewPendingIntent =
+                PendingIntent.getActivity(this, 0, viewIntent, 0);
+
+        NotificationCompat.Builder notificationBuilder =
+                new NotificationCompat.Builder(this)
+                        .setSmallIcon(R.drawable.ic_launcher)
+                        .setContentTitle("Simple")
+                        .setContentText("Hello Wear!")
+                        .setDefaults(NotificationCompat.DEFAULT_ALL)
+                        .setContentIntent(viewPendingIntent);
+
+        // Get an instance of the NotificationManager service
+        NotificationManagerCompat notificationManager =
+                NotificationManagerCompat.from(this);
+
+        // Build the notification and issues it with notification manager.
+        notificationManager.notify(notificationId, notificationBuilder.build());
     }
 
 }
